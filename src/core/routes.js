ba957fee42cc4297c60784e90a45660e6b32d22d
Sofya <sofigrekal@gmail.com>
2024-08-06 21:59:20 +0200

    fix: update routing structure

--- src/core/routes.js
+++ src/core/routes.js
@@ -1,10 +1,10 @@
-export const toMovieList = () => "movies";
+export const toMovieList = () => "/movies";
 
 export const toMovieDetails = ({ movieId } = { movieId: ":movieId" }) =>
-  `${movieId}`;
+  `/movies/${movieId}`;
 
-export const toPeopleList = () => "people";
+export const toPeopleList = () => "/people";
 
-export const toPersonDetails = ({ personId } = { personId: ":personId" }) => `${personId}`;
+export const toPersonDetails = ({ personId } = { personId: ":personId" }) => `/people/${personId}`;
 
 export const catchAllPath = () => "*";
