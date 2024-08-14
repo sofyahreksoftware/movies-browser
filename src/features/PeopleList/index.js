f289cfebad3664f8f9a63d0fbaf1adf571c34704
Sofya <sofigrekal@gmail.com>
2024-08-14 22:43:29 +0200

    feat: implement ListItemTile component for people list construction

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -12,7 +12,7 @@ import {
 } from "./peopleSlice";
 import { LoadingPage } from "../../common/LoadingPage";
 import { ErrorPage } from "../../common/ErrorPage";
-import { PersonTile } from "../../common/PersonTile";
+import { ListItemTile } from "../../common/ListItemTile";
 import { Pagination } from "../../Pagination";
 import { useQueryParam } from "../../Navigation/queryParam";
 import paginationParamName from "../../Pagination/paginationParamName";
@@ -64,14 +64,15 @@ export const PeopleList = () => {
             {peopleList.map(({ id, profile_path, name }) => (
               <StyledLink to={toPersonDetails({ personId: id })} key={nanoid()}>
                 <Item key={nanoid()}>
-                  <PersonTile
+                  <ListItemTile
+                    $small
                     {...(profile_path && {
                       poster: getImageUrl({
                         size: "/w185",
                         path: `/${profile_path}`,
                       }),
                     })}
-                    personName={name}
+                    title={name}
                   />
                 </Item>
               </StyledLink>
