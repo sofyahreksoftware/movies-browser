84b170be76c8101edbda4f1c47ae83f196df20cd
Sofya <sofigrekal@gmail.com>
2024-08-14 14:22:42 +0200

    feat: create generic details slice for person and movie pages

---
+++ src/common/detailsPage/detailSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const createDetailsSlice = ({ name }) => {
+  const slice = createSlice({
+    name,
+    initialState: { status: "initial", details: [], credits: [] },
+    reducers: {
+      fetch: (state) => {
+        state.status = "loading";
+      },
+
+      fetchSuccess: (state, action) => {
+        state.status = "success";
+        state.details = action.payload.detailsData;
+        state.credits = action.payload.creditsData;
+      },
+
+      fetchError: (state) => {
+        state.status = "error";
+        state.details = null;
+      },
+
+      clearOnLeave: (state) => {
+        state.details = [];
+      },
+    },
+  });
+
+  return slice;
+};
