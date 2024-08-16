73542cf5d663f5af8e16e819ef7b37967401b57c
Sofya <sofigrekal@gmail.com>
2024-08-16 13:39:35 +0200

    fix: correct RatingBar display when thre are no votes

--- src/common/Tile/MetaData/RatingBox/index.js
+++ src/common/Tile/MetaData/RatingBox/index.js
@@ -2,8 +2,9 @@ import { Wrapper, StarIcon, Rating, MaxRating, TotalVotes } from "./styled";
 
 export const RatingBox = ({ $small, $detailed, mark, votes }) => (
   <Wrapper $detailed={$detailed}>
-    <StarIcon hidden={$small} />
-    <Rating>{mark}</Rating><MaxRating hidden={!$detailed}>/{" "}10</MaxRating>
+    <StarIcon hidden={$small || !votes} />
+    <Rating hidden={!votes}>{mark}</Rating>
+    <MaxRating hidden={!$detailed}>/ 10</MaxRating>
     <TotalVotes hidden={$small} $detailed={$detailed}>
       {votes ? `${votes} vote${votes === 1 ? "" : "s"}` : "No votes yet"}
     </TotalVotes>
