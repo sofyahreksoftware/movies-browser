2f121f05dd7ed8dff9ee781b8b58b492e6864a1c
Sofya <sofigrekal@gmail.com>
2024-08-19 15:21:42 +0200

    refactor: import new genreic components into MovieList

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -11,14 +11,20 @@ import {
   fetchMovieSearch,
   selectMovieResult,
 } from "./moviesSlice";
+import { toMovieDetails } from "../../core/routes";
+
 import { LoadingPage } from "../../common/LoadingPage";
+import { NoResultsPage } from "../../common/NoResultsPage";
 import { ErrorPage } from "../../common/ErrorPage";
+import { Page } from "../../common/Page";
+import { Article } from "../../common/Article";
 import { Tile } from "../../common/Tile";
+
 import { Pagination } from "../../Pagination";
 import { useQueryParam } from "../../Navigation/queryParam";
 import paginationParamName from "../../Pagination/paginationParamName";
 import searchQueryName from "../../Navigation/searchQueryName";
-import { NoResultsPage } from "../../common/NoResultsPage";
+
 import {
   StyledMain,
   StyledHeader,
@@ -26,13 +32,14 @@ import {
   StyledLink,
   Item,
 } from "../styled";
-import { toMovieDetails } from "../../core/routes";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
+
   const fetchDataStatus = useSelector(selectFetchDataStatus);
   const movieList = useSelector(selectMovieList);
   const genreList = useSelector(selectGenreList);
+
   const page = useQueryParam(paginationParamName) || 1;
   const query = useQueryParam(searchQueryName) || "";
   const totalResult = useSelector(selectMovieResult);
@@ -47,6 +54,7 @@ export const MovieList = () => {
       page: page,
       type: "movie",
     };
+
     if (query) {
       dispatch(fetchMovieSearch(options));
     } else {
@@ -55,40 +63,16 @@ export const MovieList = () => {
   }, [page, dispatch, query]);
 
   return (
-    <StyledMain>
+    <main>
       {fetchDataStatus === "loading" && <LoadingPage title={query} />}
       {totalResult === 0 && query !== "" && <NoResultsPage title={query} />}
       {fetchDataStatus === "error" && <ErrorPage />}
       {fetchDataStatus === "success" && totalResult !== 0 && (
-        <>
-          <StyledHeader>{title}</StyledHeader>
-          <StyledList>
-            {movieList.map((movie) => (
-              <Item key={nanoid()}>
-                <StyledLink
-                  to={toMovieDetails({ movieId: movie.id })}
-                  key={nanoid()}
-                >
-                  <Tile
-                    poster={getImageUrl({
-                      path: `/${movie.poster_path}`,
-                      size: "/w342",
-                    })}
-                    title={movie.title}
-                    year={movie.release_date.split("-")[0]}
-                    genres={movie.genre_ids.map((id) =>
-                      genreList.find((genre) => genre.id === id)
-                    )}
-                    mark={movie.vote_average.toFixed(1).replace(".", ",")}
-                    votes={movie.vote_count}
-                  />
-                </StyledLink>
-              </Item>
-            ))}
-          </StyledList>
+        <Page>
+          <Article title={title} movies={movieList} genreList={genreList} />
           <Pagination />
-        </>
+        </Page>
       )}
-    </StyledMain>
+    </main>
   );
 };
