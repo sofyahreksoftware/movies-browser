8340ab038caeba31c028eb494f509a38fd297293
Sofya <sofigrekal@gmail.com>
2024-08-26 14:18:19 +0200

    fix: correct header margins

--- src/common/bothPageTypes/Header/index.js
+++ src/common/bothPageTypes/Header/index.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const Header = styled.h2`
-  margin: 0 0 32px;
+  margin: 0 auto 32px;
   font-weight: 600;
   font-size: 36px;
   line-height: 1.2;
+  max-width: 1368px;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     margin: 0 0 12px;
