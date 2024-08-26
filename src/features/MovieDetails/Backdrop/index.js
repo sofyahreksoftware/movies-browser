41a08da81f12f01abd2b0eae16d4b4d9f92868bb
Sofya <sofigrekal@gmail.com>
2024-08-26 14:32:35 +0200

    refactor: add conditional backdrop rendering

--- src/features/MovieDetails/Backdrop/index.js
+++ src/features/MovieDetails/Backdrop/index.js
@@ -3,22 +3,23 @@ import { getImageUrl } from "../../../common/api/getImageUrl";
 import { MetaData } from "../../../common/bothPageTypes/Tile/MetaData";
 import { Background, Poster } from "./styled";
 
-export const Backdrop = ({ movie }) => (
-  <>
-    <Background />
-    <Poster
-      backdrop={getImageUrl({
-        size: "/w1280",
-        path: `${movie?.backdrop_path}`,
-      })}
-    >
-      <MetaData
-        $detailed
-        $backdrop
-        title={movie?.original_title}
-        mark={movie.vote_average}
-        votes={movie.vote_average}
-      />
-    </Poster>
-  </>
-);
+export const Backdrop = ({ movie }) =>
+  movie && (
+    <>
+      <Background />
+      <Poster
+        backdrop={getImageUrl({
+          size: "/w1280",
+          path: `${movie?.backdrop_path}`,
+        })}
+      >
+        <MetaData
+          $detailed
+          $backdrop
+          title={movie?.original_title}
+          mark={movie.vote_average}
+          votes={movie.vote_average}
+        />
+      </Poster>
+    </>
+  );
