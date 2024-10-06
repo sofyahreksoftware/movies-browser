63b5bd47921d6be68638cb91350b236ba2d99820
Sofya <sofigrekal@gmail.com>
2024-10-06 13:40:46 +0200

    refactor: integrate reusable Details component with PersonDetails

--- src/common/detailsPages/index.js
+++ src/common/detailsPages/index.js
@@ -14,6 +14,7 @@ function Details({
   selectDetails,
   selectCredits,
   id,
+  genres,
   entityType,
 }) {
   const dispatch = useDispatch();
@@ -28,34 +29,74 @@ function Details({
     return () => {
       dispatch(clearOnLeave());
     };
-  }, [dispatch, id]);
+  }, [dispatch, id, clearOnLeave, fetch]);
 
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const [year, month, day] = dateString.split("-");
+    return `${day}.${month}.${year}`;
+  };
+  console.log(details);
   return (
     <section>
       <Page status={status}>
         <Tile
           $detailed
-          {...(entityType === "person" && { $personDetailed: true })}
-          poster={getImageUrl({
-            size: "/w342",
-            path: `${details?.poster_path}`,
+          {...(entityType === "person" && {
+            $personDetailed: true,
+            poster: getImageUrl({
+              size: "/h632",
+              path: `/${details?.profile_path}`,
+            }),
+            title: details?.name,
+            dateOfBirth: formatDate(details?.birthday),
+            placeOfBirth: details?.place_of_birth,
+            description: details?.biography,
           })}
-          title={details?.title}
-          year={details?.release_date?.split("-")[0]}
-          productionPlaces={details?.production_countries}
-          releaseDate={details?.release_date?.split("-").reverse().join(".")}
-          genres={details.genres?.map(({ name }) => name)}
-          mark={details?.vote_average?.toFixed(1)}
-          votes={details?.vote_count}
-          description={details?.overview}
+          {...(entityType === "movie" && {
+            poster: getImageUrl({
+              size: "/w342",
+              path: `${details?.poster_path}`,
+            }),
+            title: details?.title,
+            year: details?.release_date?.split("-")[0],
+            productionPlaces: details?.production_countries,
+            releaseDate: details?.release_date?.split("-").reverse().join("."),
+            genres: details?.genres?.map(({ name }) => name),
+            mark: details?.vote_average?.toFixed(1),
+            votes: details?.vote_count,
+            description: details?.overview,
+          })}
         />
+
         {credits.cast?.length !== 0 && (
-          <Article title="Cast" people={credits.cast} />
+          <Article
+            {...(entityType === "movie" && {
+              title: "Cast",
+              people: credits.cast,
+            })}
+            {...(entityType === "person" && {
+              title: "Movies - cast",
+              movies: credits.cast,
+              genreList: genres,
+            })}
+          />
         )}
 
-        {credits.crew?.length !== 0 && (
-          <Article title="Crew" people={credits.crew} />
-        )}
+        {credits.crew?.length !== 0 &&
+          ({
+            ...(entityType === "movie" && {
+              title: "Crew",
+              people: credits.crew,
+            }),
+          },
+          {
+            ...(entityType === "person" && {
+              title: "Movies - crew",
+              movies: credits.crew,
+              genreList: genres,
+            }),
+          })}
       </Page>
     </section>
   );
