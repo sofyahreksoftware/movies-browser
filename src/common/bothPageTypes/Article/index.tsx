dd2182c25942e3c70fdae749823158dfdadc4e9b
Sofya <sofigrekal@gmail.com>
2024-11-25 16:07:58 +0100

    refactor(article): simplify Article component syntax to arrow function and remove console.log statements

--- src/common/bothPageTypes/Article/index.tsx
+++ src/common/bothPageTypes/Article/index.tsx
@@ -6,16 +6,13 @@ import { Header } from "../Header";
 
 type ArticleProps = XOR<MovieProps, PeopleProps>;
 
-export const Article = ({ title, people, movies, genreList }: ArticleProps) => {
-  console.log(genreList);
-  return (
-    <Wrapper>
-      {title && <Header>{title}</Header>}
-      {movies && genreList ? (
-        <List people={people} movies={movies} genreList={genreList} />
-      ) : people ? (
-        <List people={people} movies={movies} genreList={genreList} />
-      ) : null}
-    </Wrapper>
-  );
-};
+export const Article = ({ title, people, movies, genreList }: ArticleProps) => (
+  <Wrapper>
+    {title && <Header>{title}</Header>}
+    {movies && genreList ? (
+      <List people={people} movies={movies} genreList={genreList} />
+    ) : people ? (
+      <List people={people} movies={movies} genreList={genreList} />
+    ) : null}
+  </Wrapper>
+);
