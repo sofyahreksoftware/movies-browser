a2a91c80ba7b2895a43b6c88ef813a6929bc8fe4
Sofya <sofigrekal@gmail.com>
2025-01-08 23:25:49 +0100

    refactor: remove unnecessyry types

--- src/common/bothPageTypes/moviePersonTypes.ts
+++ src/common/bothPageTypes/moviePersonTypes.ts
@@ -9,77 +9,50 @@ export type CharacterProp = string | undefined;
 export type YearProp = string | undefined;
 export type MarkProp = number | undefined;
 export type VoteAverageProp = number | undefined;
-export type GenreProp = string | undefined;
+export type GenreProp = string;
 export type Genres = {
-  [key: number]: GenreProp | undefined;
+  [key: number]: GenreProp;
 };
-export type PathProp = string;
+export type PathProp = string | undefined;
 export type IdProp = string;
 export type NameTitleProp = string;
-export type OverviewProp = string;
+export type OverviewDescriptionProp = string;
+export type EntityTypeProp = "movie" | "person";
 
-export interface CastMember {
-  adult: boolean;
-  gender: number | null; // Możliwe wartości: 0, 1, 2, lub null
-  id: number;
-  known_for_department: string;
+export interface Person {
+  id: IdProp;
+
   name: NameTitleProp;
-  original_name: NameTitleProp;
-  popularity: number;
-  profile_path: string | null;
-  credit_id: string;
-  order?: number;
-  cast_id?: number; // Nie zawsze występuje
-  character?: string; // Tylko dla obsady
-}
+  profile_path: PathProp;
+  character: CharacterProp;
 
-export interface CrewMember
-  extends Omit<CastMember, "cast_id" | "character" | "order"> {
-  department: string;
-  job: string;
+  birthday: DateOfBirthProp;
+
+  place_of_birth: PlaceOfBirthProp;
+  biography: OverviewDescriptionProp;
+  poster_path: PathProp;
 }
 
 export interface Movie {
   backdrop_path: PathProp;
   character: CharacterProp;
   genre_ids: number[];
-  id: IdProp;
+  id: number;
   original_title: NameTitleProp;
-  overview: OverviewProp;
+  overview: OverviewDescriptionProp;
   poster_path: PathProp;
   release_date: ReleaseDateProp; // from API
-  releaseDate: ReleaseDateProp; // optional alias for vote_average
-  year: YearProp;
   title: NameTitleProp;
   vote_average: VoteAverageProp; // from API
-  mark?: MarkProp; // optional alias for vote_average
-  vote_count: VoteAverageProp;
-  votes: VoteAverageProp;
-  productionPlaces: ProductionPlaces;
-  genres: Genres;
-}
 
-export interface Person {
-  id: IdProp;
-  job: string;
-  name: NameTitleProp;
-  profile_path: PathProp;
-  character: CharacterProp;
-  dateOfBirth: DateOfBirthProp;
-  placeOfBirth: PlaceOfBirthProp;
+  vote_count: VoteAverageProp;
+
+  production_countries: ProductionPlaces;
+
+  genres: Genres;
 }
 
 export type Movies = Movie[];
 export type People = Person[];
-export type CastCrew = { cast: CastMember[]; crew: CrewMember[] };
 
-export interface MovieProps {
-  title?: NameTitleProp;
-  movies: Movies;
-  genreList: Genres;
-}
-
-export interface PeopleProps {
-  title?: NameTitleProp;
-  people: People;
-}
+export type CastCrew = { cast: Movies | People; crew: Movies | People };
