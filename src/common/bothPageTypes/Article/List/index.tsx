e7edb76c2359587a911ae8d75c87865d07c61277
Sofya <sofigrekal@gmail.com>
2025-01-11 23:37:23 +0100

    refactor: use person.id and movie.id as keys, remove redundant code in List component

--- src/common/bothPageTypes/Article/List/index.tsx
+++ src/common/bothPageTypes/Article/List/index.tsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import { nanoid } from "@reduxjs/toolkit";
-
 import { getImageUrl } from "../../../api/getImageUrl";
 import { toPersonDetails, toMovieDetails } from "../../../../core/routes";
 import {
@@ -11,7 +9,6 @@ import {
   Movie,
   EntityTypeProp,
 } from "../../moviePersonTypes";
-
 import { Tile } from "../../Tile";
 
 import { Wrapper, Item } from "./styled";
@@ -22,62 +19,59 @@ interface ListProps {
   genreList?: Genres;
   entityType: EntityTypeProp;
 }
+
 export const List = ({ people, movies, genreList }: ListProps) => (
   <>
     {people && (
       <Wrapper $small>
-        {Array.isArray(people) &&
-          people?.map((person: Person) => (
-            <Link
-              to={toPersonDetails({ personId: person.id.toString() })}
-              key={nanoid()}
-            >
-              <Item key={nanoid()}>
-                <Tile
-                  $small
-                  poster={getImageUrl({
-                    size: "/w342",
-                    path: `${person.profile_path}.jpg`,
-                  })}
-                  title={person.name}
-                  character={person.character}
-                />
-              </Item>
-            </Link>
-          ))}
+        {people.map((person: Person) => (
+          <Link
+            to={toPersonDetails({ personId: person.id.toString() })}
+            key={person.id}
+          >
+            <Item>
+              <Tile
+                $small
+                poster={getImageUrl({
+                  size: "/w342",
+                  path: `${person.profile_path}.jpg`,
+                })}
+                title={person.name}
+                character={person.character}
+              />
+            </Item>
+          </Link>
+        ))}
       </Wrapper>
     )}
 
     {movies && (
       <Wrapper>
-        {Array.isArray(movies) &&
-          movies?.map((movie: Movie) => (
-            <Link
-              to={toMovieDetails({ movieId: movie.id.toString() })}
-              key={nanoid()}
-            >
-              <Item key={nanoid()}>
-                <Tile
-                  poster={getImageUrl({
-                    size: "/w342",
-                    path: movie.poster_path,
-                  })}
-                  title={movie.title}
-                  year={movie.release_date?.split("-")[0]}
-                  genres={
-                    movie.genre_ids &&
-                    Array.isArray(movie.genre_ids) &&
-                    genreList
-                      ? movie.genre_ids.map((id) => genreList[id])
-                      : undefined
-                  }
-                  mark={Number(movie.vote_average?.toFixed(1))}
-                  votes={movie.vote_count}
-                  character={movie.character}
-                />
-              </Item>
-            </Link>
-          ))}
+        {movies.map((movie: Movie) => (
+          <Link
+            to={toMovieDetails({ movieId: movie.id.toString() })}
+            key={movie.id}
+          >
+            <Item>
+              <Tile
+                poster={getImageUrl({
+                  size: "/w342",
+                  path: movie.poster_path,
+                })}
+                title={movie.title}
+                year={movie.release_date?.split("-")[0]}
+                genres={
+                  movie.genre_ids && genreList
+                    ? movie.genre_ids.map((id) => genreList[id])
+                    : undefined
+                }
+                mark={Number(movie.vote_average?.toFixed(1))}
+                votes={movie.vote_count}
+                character={movie.character}
+              />
+            </Item>
+          </Link>
+        ))}
       </Wrapper>
     )}
   </>
